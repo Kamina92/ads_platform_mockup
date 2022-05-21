29b66670492ba44443ecb7c41511c8ce25aa81f7
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-05-21 17:33:56 +0200

    carosello con card prodotto aggiunto refinire card prodotti

--- script.js
+++ script.js
@@ -34,3 +34,41 @@ document.addEventListener('scroll',()=>{
     }
 })
 
+
+// SWIPER
+
+const swiper = new Swiper('.swiper', {
+
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 5,
+        },
+      },
+  });
