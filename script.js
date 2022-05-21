691578a258d2444dda072bcc9f9b575f1989f78a
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-05-21 10:14:54 +0200

    eliminazione contenuto branch main

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-let toggleNav = document.querySelector('#toggleNav')
-let logIn = document.querySelector('#logIn')
-let srcBar = document.querySelector('#srcBar')
-let exitLog = document.querySelector('#exitLog')
+let navMobileCont = document.querySelector('.navMobileCont');
 
-toggleNav.addEventListener('click',()=>{
-    toggleNav.classList.toggle('fa-book');
-    toggleNav.classList.toggle('fa-book-open');
+console.log(window);
 
-})
 
-logIn.addEventListener('click',()=>{
-
-    srcBar.classList.remove('scrBar');
-    srcBar.classList.add('srcBarActive');
-
-})
-
-exitLog.addEventListener('click',()=>{
-    srcBar.classList.remove('srcBarActive');
-    srcBar.classList.add('scrBar');
-
-})
+// ## function declaration
+function scrollEventThrottle(fn) {
+    let last_known_scroll_position = 0;
+    let ticking = false;
+    window.addEventListener("scroll", function () {
+      let previous_known_scroll_position = last_known_scroll_position;
+      last_known_scroll_position = window.scrollY;
+      if (!ticking) {
+        window.requestAnimationFrame(function () {
+          fn(last_known_scroll_position, previous_known_scroll_position);
+          ticking = false;
+        });
+        ticking = true;
+      }
+    });
+  }
+  
+  // ## function invocation
+  scrollEventThrottle((scrollPos, previousScrollPos) => {
+      if (previousScrollPos > scrollPos) {
+          navMobileCont.classList.add('navMobileCont')
+        } else {
+          navMobileCont.classList.remove('navMobileCont')
+      }
+  });
