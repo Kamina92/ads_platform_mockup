160c3f2306fa5a23d8826e0c53fd984626011bfc
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-05-21 14:47:22 +0200

    marge eseguito branch test eliminato

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-let navMobileCont = document.querySelector('.navMobileCont');
-
-console.log(window);
-
-
-// ## function declaration
-function scrollEventThrottle(fn) {
-    let last_known_scroll_position = 0;
-    let ticking = false;
-    window.addEventListener("scroll", function () {
-      let previous_known_scroll_position = last_known_scroll_position;
-      last_known_scroll_position = window.scrollY;
-      if (!ticking) {
-        window.requestAnimationFrame(function () {
-          fn(last_known_scroll_position, previous_known_scroll_position);
-          ticking = false;
-        });
-        ticking = true;
-      }
-    });
-  }
-  
-  // ## function invocation
-  scrollEventThrottle((scrollPos, previousScrollPos) => {
-      if (previousScrollPos > scrollPos) {
-          navMobileCont.classList.add('navMobileCont')
-        } else {
-          navMobileCont.classList.remove('navMobileCont')
-      }
-  });
-=======
 let counterTitle = document.querySelector('#counterTitle');
 
 
@@ -66,4 +34,3 @@ document.addEventListener('scroll',()=>{
     }
 })
 
->>>>>>> test
