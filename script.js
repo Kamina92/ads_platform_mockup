29794eea53b9754b1467167b2b99477de8cac3e1
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-05-21 12:09:37 +0200

    navbar nuova 70% ok, header creato

---
+++ script.js
@@ -0,0 +1,33 @@
+let counterTitle = document.querySelector('#counterTitle');
+
+
+
+function titleInterval(num,tag,finaltime) {
+
+    let counter=num;
+
+    let interval= setInterval(()=>{
+        if(counter<finaltime){
+            counter++
+            tag.innerHTML=`${counter} ANNUNCI`;
+        }else{
+            clearInterval(interval);
+        }
+    },100);
+    
+}
+
+titleInterval(0,counterTitle,100)
+
+let navDesk=document.querySelector('#navDesk')
+let navMobile=document.querySelector('#navMobile')
+
+document.addEventListener('scroll',()=>{
+    if(window.scrollY>300){
+        navDesk.classList.add('d-lg-none');
+        navMobile.classList.add('d-none');
+    } else{
+        navDesk.classList.remove('d-lg-none');
+        navMobile.classList.remove('d-none')
+    }
+})
